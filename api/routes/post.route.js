f9d4e0511625f5fa2a9066ce04f92ead56ef1beb
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-01-18 21:32:12 +0400

    add update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,6 +4,7 @@ import {
   create,
   deletePost,
   getposts,
+  updatePost,
 } from "../controllers/post.controllers.js";
 
 const router = express.Router();
@@ -11,5 +12,5 @@ const router = express.Router();
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
 router.delete("/deletepost/:postId/:userId", verifyToken, deletePost);
-
+router.put("/updatepost/:postId/:userId", verifyToken, updatePost);
 export default router;
