5d4386eef966c1dfc216b338b3a334e33abf1aa1
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-01-18 22:45:24 +0400

    complete post page functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -70,7 +70,7 @@ export default function DashPosts() {
       console.log(error.message);
     }
   };
-  console.log(userPosts.length);
+
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar crollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 w-full">
       {currentUser.currentUser.isAdmin && userPosts?.length > 0 ? (
