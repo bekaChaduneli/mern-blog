a88325dbec7765ce9c4e3f92759982d120abca16
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-01-18 22:58:38 +0400

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,27 @@
+import { Button } from "flowbite-react";
+
+export default function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center mt-8">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">Want to see my other projects?</h2>
+        <p className="text-gray-500 my-2">Checkout my portfolio</p>
+        <Button
+          className="rounded-tl-xl rounded-bl-none"
+          gradientDuoTone="purpleToPink"
+        >
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://portfolio-six-ecru-43.vercel.app/"
+          >
+            Portfolio
+          </a>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img src="https://firebasestorage.googleapis.com/v0/b/mern-blog-f0d68.appspot.com/o/Screenshot%20from%202024-01-18%2022-56-41.png?alt=media&token=dde72b35-445f-4d73-a853-faa6866c0163" />
+      </div>
+    </div>
+  );
+}
