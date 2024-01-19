606963bf58eedb43b37912a898596cc7df9436b1
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-01-19 23:46:42 +0400

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,28 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Beka's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Beka's Blog! This blog was created by Beka Chaduneli as
+              a personal project to share his thoughts and ideas with the world.
+              Beka is a passionate developer who loves to write about
+              technology, coding, and everything in between. On this blog,
+              you'll find weekly articles and tutorials on topics such as web
+              development, software engineering, and programming languages. Beka
+              is always learning and exploring new technologies, so be sure to
+              check back often for new content! We encourage you to leave
+              comments on our posts and engage with other readers. You can like
+              other people's comments and reply to them as well. We believe that
+              a community of learners can help each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,3 +1,13 @@
+import CallToAction from "../components/CallToAction";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        You can check all my projects in my portfolio
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
